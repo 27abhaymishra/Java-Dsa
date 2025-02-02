698ab0faaab5a07d2e4c9832fd874d7af834a7ed
Abhay mishra <126569073+27abhaymishra@users.noreply.github.com>
2025-02-02 15:31:19 +0530

    Add files via upload

---
+++ Arrays/array3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class array3 {
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+    int arrr [] = new int [n];
+    for(int i = 0; i<n; i++){
+        arrr[i] = sc.nextInt();
+    }
+ int arr [] = new int[-1];
+    // for(int x : arr ){
+    //          sum += x;
+    // }
+    // int sum = Arrays.stream(arr).sum();
+    // System.out.println(sum);
+    // Arrays.sort(arr);
+    // for(int x : arr){
+    //     System.out.println(x);
+    // }
+    // System.out.println(arr);
+    
+   } 
+}

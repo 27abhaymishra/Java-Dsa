ca45093b9021c170ea165de56d4e35ea7def4a6a
Abhay mishra <126569073+27abhaymishra@users.noreply.github.com>
2025-02-22 10:17:36 +0530

    Add files via upload

---
+++ ArrayList/class1.java
@@ -0,0 +1,30 @@
+package arraylist;
+import java.util.*;
+public class class1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> li = new ArrayList<>();
+    li.add(1);
+    li.add(2);
+    li.add(6);
+//     System.out.println(li.size());
+// li.add(2,5);
+// System.out.println(li);
+// System.out.println(li.get(1));
+// li.add(0,10);
+// System.out.println(li.get(0));
+// System.out.println(li);
+// li.remove(0);
+// System.out.println(li.remove(2));
+// System.out.println(li);
+// // System.out.println(li.get(5));
+// // System.out.println(li.remove(5));
+// System.out.println(li.isEmpty());
+// System.out.println(li.contains(1));
+// System.out.println(li.contains(11));
+
+for(int i =0; i<li.size(); i++){
+    System.out.println(li.get(i));
+}
+}
+}
+

64aecb21c241b74caefcbf0dff46778e8e3a3342
Abhay mishra <126569073+27abhaymishra@users.noreply.github.com>
2025-02-22 10:17:54 +0530

    Add files via upload

---
+++ ArrayList/class2.java
@@ -0,0 +1,9 @@
+package arraylist;
+import java.util.*;
+//Generic class
+public class class2 {
+    public static void main(String[] args) {
+        
+    ArrayList     li = new ArrayList<>();
+    }
+}
